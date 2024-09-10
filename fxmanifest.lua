c824d3a5cbfa5b7b2e56db9c96cd7ca48a4527dc
AdrianCeku <cekuadrian4@gmail.com>
2024-09-10 15:53:11 +0200

    setup

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'github.com/AdrianCeku'
+
+description 'ultimate5-ui'
+
+version '1.0'
+
+lua54 'yes'
+
+ui_page {
+	'html/index.html',
+}
+
+files {
+
+}
+
+shared_scripts{
+
+}
+
+client_scripts{
+
+}
+
+server_scripts{
+
+}
