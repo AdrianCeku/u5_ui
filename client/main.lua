dfc4d0dc09c606bf042cd212209356ea2bcff8d5
AdrianCeku <cekuadrian4@gmail.com>
2024-09-12 01:38:23 +0200

    added functions support

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,15 @@
 u5_ui = {}
 
+local WAITING_FOR_SECTION_ID = false
+local SECTION_ID = nil
+
+local WAITING_FOR_COMPONENT_ID = false
+local COMPONENT_ID = nil
+
+local onClickFunctions = {}
+
 function u5_ui.addSection(components, options)
+    WAITING_FOR_SECTION_ID = true
     SendNUIMessage({
                     type = "addSection",
                     section = {
@@ -8,9 +17,65 @@ function u5_ui.addSection(components, options)
                         options = options
                     }
                 })
+                
+    while WAITING_FOR_SECTION_ID do
+        Citizen.Wait(10)
+    end
+
+    local retVal = SECTION_ID
+    SECTION_ID = nil
+    
+    return retVal
 end
 
-exports("get", function()
+function u5_ui.addComponentToSection(sectionId, componentType, props, onClick)
+    local onClickId = #onClickFunctions + 1
+    onClickFunctions[onClickId] = onClick
+
+    local component = {
+        type = componentType,
+        props = props,
+        onClickId = onClickId
+    }
+
+    WAITING_FOR_COMPONENT_ID = true
+    SendNuiMessage({
+        type = "addComponentToSection",
+        sectionId = sectionId,
+        component = component
+    })
+
+    while WAITING_FOR_COMPONENT_ID do
+        Citizen.Wait(10)
+    end
+
+    local retVal = COMPONENT_ID
+    COMPONENT_ID = nil
+
+    return retVal
+end
+
+RegisterNUICallback('sendSectionId', function(data, cb)
+    SECTION_ID = data.id
+    WAITING_FOR_SECTION_ID = false
+
+    cb('ok')
+end)
+
+RegisterNUICallback('sendComponentId', function(data, cb)
+    COMPONENT_ID = data.id
+    WAITING_FOR_COMPONENT_ID = false
+
+    cb('ok')
+end)
+
+RegisterNUICallback('clickTriggered', function(data, cb)
+    onClickFunctions[data.id]()
+    
+    cb('ok')
+end)
+
+exports("getObject", function()
     return u5_ui
 end)
 
@@ -39,7 +104,7 @@ Citizen.CreateThread(function()
                 description = "This is a test"
             }
 
-            u5_ui.addSection(components, options)
+            local section = u5_ui.addSection(components, options)
             SetNuiFocus(true, true)
             Wait(5000)
             SetNuiFocus(false, false)
--- ui/src/App.vue
+++ ui/src/App.vue
@@ -8,45 +8,102 @@ interface ComponentMap {
   [key: string]: any;
 }
 const componentMap: ComponentMap = {
- "Button": Button,
+  "Button": Button,
   "Accordion": Accordion
 }
 
 interface Section {
   options: {
     title: string
+    xAlign: "left" | "center" | "right"
+    yAlign: "top" | "center" | "bottom"
+    width: "full" | "half" | "third" | "quarter"
+    height: "full" | "half" | "third" | "quarter"
+    display: "flex" | "grid" | "block" | "hidden"
+    classes: string[]
   }
-  components: {
-    type: string
-    props: {
-      [key: string]: any
+  components: [
+    {    
+      type: string
+      props: {
+        [key: string]: any
+      }
+      onClickId: number
     }
-  }[]
+  ]
 }
 
 const sections = ref<Section[]>([])
 
+
 window.addEventListener('message', (event) => {
-  console.log(event.data)
   if (event.data.type === 'addSection') {
     sections.value.push(event.data.section)
-    console.log(sections.value)
+    
+    fetch(`https://u5_ui/sendSectionId`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+          id: sections.value.length - 1
+      })
+    });
   }
+
+  else if (event.data.type === 'updateSection') {
+    sections.value[event.data.id] = event.data.section
+  }
+
+  else if (event.data.type === "addComponentToSection") {
+    sections.value[event.data.sectionId].components.push(event.data.component)
+
+    fetch(`https://u5_ui/sendComponentId`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+          id: sections.value[event.data.sectionId].components.length - 1
+      })
+    });
+  }
+
+  else if (event.data.type === "updateComponent") {
+    sections.value[event.data.sectionId].components[event.data.componentId] = event.data.component
+  }
+
 })
+
+function clickTriggered(id: number) {
+  fetch(`https://u5_ui/clickTriggered`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+        id: id
+    })
+  });
+}
 
 </script>
 
 <template>
   <div>
-    <div v-for="(section, index) in sections" :key="index">
-      <h1>{{ section.options.title }}</h1>
+    <div 
+      v-for="(section, sectionId) in sections" 
+      :key="sectionId" 
+      :class="[section.options.classes]"
+    >
+      <h1 :v-if="section.options.title">{{ section.options.title }}</h1>
       <component
-        v-for="(comp, compIndex) in section.components"
-        :key="compIndex"
-        :is="componentMap[comp.type]"
-        v-bind="comp.props"
+        v-for="(component, componentId) in section.components"
+        :key="componentId"
+        :is="componentMap[component.type]"
+        v-bind="component.props"
+        @click="component.onClickId ? clickTriggered(component.onClickId) : null"
       >
-      {{ comp.props.text }}
       </component>
     </div>
   </div>
